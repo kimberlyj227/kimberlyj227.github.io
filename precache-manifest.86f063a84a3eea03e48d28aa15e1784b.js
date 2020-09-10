5ac780b247eeb6a0511c9bf8c7480dcf648cfe1e
Kimberly Owens <kimberly.j.owens@gmail.com>
2020-09-09 21:41:21 -0600

    Updates

---
+++ precache-manifest.86f063a84a3eea03e48d28aa15e1784b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c88f732ad21019cfd7c46558dd8d3e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "387b893a95e149cb1847",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "e7b7b9d3bb2d2e2b6148",
+    "url": "/static/css/main.31be524c.chunk.css"
+  },
+  {
+    "revision": "387b893a95e149cb1847",
+    "url": "/static/js/2.0b1590f7.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.0b1590f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7b7b9d3bb2d2e2b6148",
+    "url": "/static/js/main.1284c918.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "75a288e1c9acc974d21870d63923330f",
+    "url": "/static/media/heroImg.75a288e1.png"
+  },
+  {
+    "revision": "930b836ee69a315be2cad5580cddf9e6",
+    "url": "/static/media/kimberly.owens.resume.930b836e.pdf"
+  },
+  {
+    "revision": "44f853b0ea91d433d95e9961c09fafbf",
+    "url": "/static/media/logo.44f853b0.png"
+  }
+]);
