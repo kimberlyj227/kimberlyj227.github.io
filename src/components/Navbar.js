bfb3bd55432870566e7a62729b203a13d2d5b322
Kimberly Owens <kimberly.j.owens@gmail.com>
2020-09-09 21:38:30 -0600

    updated fonts for headers

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,14 +4,14 @@ import "../styles/Navbar.css";
 
 const Nav = () => {
   return (
-    <div id="nav">
+    <nav>
       <h4 
         className="nav"> 
         <a href="#about" className="link"> ABOUT </a>   ||    
         <a href="#portfolio" className="link"> PORTFOLIO </a>   ||    
         <a href="#contact" className="link"> CONTACT </a>
       </h4>
-    </div>
+    </nav>
   )
 }
 
